f9ff0cb4701b40b3779ff23cfb2ea30222aa5e53
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-07-01 12:01:53 +0530

    Created Node and LinkedList factory functions with some methods in it

---
+++ linkedList.js
@@ -0,0 +1,53 @@
+const LinkedList = () => {
+  const append = (value) => {};
+
+  const prepend = (value) => {};
+
+  const size = () => {};
+
+  const head = () => {};
+
+  const tail = () => {};
+
+  const at = (index) => {};
+
+  const pop = () => {
+    // remove last element
+  };
+
+  const contains = (value) => {
+    // return true if value is present
+  };
+
+  const find = (value) => {
+    // return index of the node
+  };
+
+  const toString = () => {
+    // return linked list as string
+  };
+
+  const insertAt = (value, index) => {};
+
+  const removeAt = (index) => {};
+
+  return {
+    append,
+    prepend,
+    size,
+    head,
+    tail,
+    at,
+    pop,
+    contains,
+    find,
+    toString,
+    insertAt,
+    removeAt,
+  };
+};
+
+const Node = (value) => {
+  value = value || null;
+  nextNode = null;
+};
